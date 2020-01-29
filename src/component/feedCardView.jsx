59213612bfaa700998e3c1a6f6c46a3257bb0383
임동완 <limson_95@naver.com>
2020-01-29 23:03:37 +0900

    cardView commit

--- src/component/feedCardView.jsx
+++ src/component/feedCardView.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PundingProgress from './pundingProgress';
+
 
 const feedCardView = () => {
     return (
@@ -6,8 +8,19 @@ const feedCardView = () => {
             <div className="card_view_img_div">
                 <img className="card_view_img" src="https://via.placeholder.com/150"></img> 
             </div>
-            <div className="card_view_user_profile_img_div">
+            <div className="card_view_user_profile_img_div card_view_punding_left_margin">
                 <img className="card_view_user_profile_img" src="https://via.placeholder.com/150"></img>
+            </div>
+            <div className="card_view_punding_information_div card_view_punding_left_margin">
+                <p>
+                    펀딩 타이틀은 최대 2줄이며,<br/> 2줄 이상부터는 ellipsis 처리 됩니다. <br/> 이것은 3번째 줄입니다.
+                </p>
+            </div>
+            <div className="card_view_punding_user_name_div card_view_punding_left_margin">
+                <p>메이커 닉네임</p>
+            </div>
+            <div className="card_view_punding_left_margin">
+                <PundingProgress/>
             </div>
         </div>
     );
---
+++ src/component/pundingProgress.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const pundingProgress = () => {
+    return (
+        <div>
+            <div className="punding_progress_bar_div">
+                <div className="punding_progress_bar">
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default pundingProgress;
