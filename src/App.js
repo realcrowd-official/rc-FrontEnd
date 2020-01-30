cdde02d59f7ae97c48ebf76fef9cf112ef0d2c46
임동완 <limson_95@naver.com>
2020-01-30 16:23:51 +0900

    react-router-dom init

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.scss';
 import Home from './container/Home'
 import BottomSheet from './container/Bottomsheet'
 import { TabProvider } from './context/tab';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <TabProvider>
-        <Home/>
-      </TabProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* <Feed/> */}
+        </Route>
+        <Route path="/project" children={
+          <TabProvider>
+            <Home/>
+          </TabProvider>
+        }/>
+      </Switch>
       <BottomSheet/>
-    </>
+    </Router>
   );
 }
 
