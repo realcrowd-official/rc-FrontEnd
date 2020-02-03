3546913dd261d3b5443e9fa17c8db310836cb539
임동완 <limson_95@naver.com>
2020-02-03 18:33:45 +0900

    철자 오류 수정

--- src/component/feedCardView.jsx
+++ src/component/feedCardView.jsx
@@ -28,22 +28,22 @@ const FeedCardView = () => {
             <Slick className="margin_auto width_ninth height_ninth feed_card_view_image_slick">
                 <img className="feed_card_view_image width_ninth height_ninth" src="https://via.placeholder.com/150"/>
             </Slick>
-            <div className="feed_card_view_punding_div">
-                <img className="feed_card_view_punding_img" src="https://via.placeholder.com/150" alt=""/>
-                <div className="feed_card_view_punding_header_text_div">
-                    <p className="feed_card_view_punding_title">이미지 크기 고정, 타이틀 텍스트 너비 가변, 최대 3줄 이후 ellipsis 3세줄테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트3줄 테스트</p>
-                    <p className="feed_card_view_punding_user">메이커 닉네임</p>
+            <div className="feed_card_view_funding_div">
+                <img className="feed_card_view_funding_img" src="https://via.placeholder.com/150" alt=""/>
+                <div className="feed_card_view_funding_header_text_div">
+                    <p className="feed_card_view_funding_title">이미지 크기 고정, 타이틀 텍스트 너비 가변, 최대 3줄 이후 ellipsis 3세줄테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트 3줄 테스트3줄 테스트</p>
+                    <p className="feed_card_view_funding_user">메이커 닉네임</p>
                 </div>
             </div>
             <FundingProgress/>
-            <div className="feed_card_view_punding_explain_div">
-                <p className="feed_card_view_punding_explain_text">피드 카드의 본문 내용은 최대 3줄까지 숨김 없이 보여지며, 3줄이 넘어가게 될 경우에는 다음 예시와 같이 ellipsis 더 보기 처리가 됩니다 ...</p>
+            <div className="feed_card_view_funding_explain_div">
+                <p className="feed_card_view_funding_explain_text">피드 카드의 본문 내용은 최대 3줄까지 숨김 없이 보여지며, 3줄이 넘어가게 될 경우에는 다음 예시와 같이 ellipsis 더 보기 처리가 됩니다 ...</p>
             </div>
             {/* 더보기는 추후 구현 */}
-            <div className="feed_card_view_punding_like_comment_div">
-                <div className="feed_card_view_punding_like_comment_p"><span>좋아요 96 ㆍ 댓글 32</span></div>
+            <div className="feed_card_view_funding_like_comment_div">
+                <div className="feed_card_view_funding_like_comment_p"><span>좋아요 96 ㆍ 댓글 32</span></div>
             </div>
-            <div className="feed_card_view_punding_icon_div">
+            <div className="feed_card_view_funding_icon_div">
                 <img src={icHeart} alt=""/>
                 <img src={icComment} alt=""/>
                 <img src={icShare} alt=""/>
