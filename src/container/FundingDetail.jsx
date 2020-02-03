8ea7f83a8327a7d6d5c4f0de93e0e7aac0e8e97d
임동완 <limson_95@naver.com>
2020-02-03 23:26:15 +0900

    ...

--- src/container/FundingDetail.jsx
+++ src/container/FundingDetail.jsx
@@ -26,6 +26,37 @@ const FundingDetail = () => {
                         </button>
                     </div>
                 </div>
+
+                <div className="funding_detail_title">
+                    <p>펀딩 상세페이지 내의 펀딩 타이틀은 글줄 수의 제한이 없습니다.</p>
+                </div>
+
+                <div className="funding_detail_current_fund_div">
+                    <div>
+                        <div>
+                            <p className="funding_detail_current_fund_fix_text">모인 후원금</p>
+                        </div>
+                        <div>
+                            <p className="funding_detail_current_fund_change_text">339,000원</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p className="funding_detail_current_fund_fix_text">후원 종료까지</p>
+                        </div>
+                        <div >
+                            <p className="funding_detail_current_fund_change_text">14일</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p className="funding_detail_current_fund_fix_text">후원자</p>
+                        </div>
+                        <div>
+                            <p className="funding_detail_current_fund_change_text">128명</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             
         </div>
