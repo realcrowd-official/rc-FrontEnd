aa414287a07d25c444cf3f8eab3aaf2459fb5752
Hyouk Seo <ghsspower@naver.com>
2020-02-18 00:09:44 +0900

    twitterShare 함수 추가

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -1,2 +1,15 @@
 export const numberWithCommas = (x: number) =>
   x && x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const twitterShare = (title: string, url: string) => {
+  let escapeAmpersand = (text: string) => {
+    return text.replace(/\&/g, '%26').replace(/&amp;/g, '%26');
+  };
+
+  window.open(
+    `https://twitter.com/intent/tweet?text=${escapeAmpersand(
+      title
+    )}%0a${escapeAmpersand(url)}`,
+    '_blank'
+  );
+};
