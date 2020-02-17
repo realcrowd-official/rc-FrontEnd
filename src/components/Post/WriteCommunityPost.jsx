36e955d8a95270d6da491a49535fceb79e3faa5b
임동완 <limson_95@naver.com>
2020-02-18 02:12:20 +0900

    [ADD] 커뮤니티 글작성 컴포넌트 작성중

--- src/components/Post/WriteCommunityPost.jsx
+++ src/components/Post/WriteCommunityPost.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 import ActionBtn from '@/components/ActionBtn';
 
+import CameraIcon from '@/img/post/ic-camera-grey.svg'
+
 const WriteCommunityPost = () => {
-    return (
-        <div className="wcp_div">
-            <ActionBtn aText="소중한 한마디를 전해주세요" />
+  return (
+    <div className="wcp_div">
+      {/* <ActionBtn aText="소중한 한마디를 전해주세요" /> */}
+      <div className="wcp_img_div">
+        <div className="wcp_img_add_btn">
+            <img src={CameraIcon} alt=""/>
+            <p>1/5</p>
         </div>
-    );
+        {/* <input type="image" src="https://via.placeholder.com/150" alt="" /> */}
+      </div>
+      <hr/>
+      <div className="wcp_wrtie_div">
+        <input type="text" placeholder="여기서부터 글을 작성해 주세요" name="" id="" />
+      </div>
+    </div>
+  );
 };
 
 export default WriteCommunityPost;
