dc66cee1a10c1c7642c6c8044402519097bdff78
임동완 <limson_95@naver.com>
2020-02-18 16:48:59 +0900

    [FIX] tab width 값 변경

--- src/container/Profile/MyPageHome.jsx
+++ src/container/Profile/MyPageHome.jsx
@@ -3,6 +3,7 @@
 import React, { useContext, useEffect } from 'react';
 import HABContext from '@/context/headerAndBottom';
 import ProfileUserInfo from '@/components/Profile/ProfileUserInfo';
+import ToTopTab from '@/components/Tab/ToTopTab';
 
 const MyPageHome = () => {
   const { state, action } = useContext(HABContext);
@@ -11,11 +12,17 @@ const MyPageHome = () => {
     action.setBottomType('true');
     action.setPath('profile');
   }, [action]);
+
+  const tabJson = [
+    { tabName: '후원내역', tabId: 'mpFunding' },
+    { tabName: '커뮤니티', tabId: 'mpCommunity' }
+  ];
   return (
     <div className="home_body">
       <div className="mph_user_div">
         <ProfileUserInfo />
       </div>
+      <ToTopTab tabJson={tabJson}/>
     </div>
   );
 };
