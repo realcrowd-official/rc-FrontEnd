40fabb5b73a73f84a2a3f62e802ba917715daca3
임동완 <limson_95@naver.com>
2020-02-18 14:41:01 +0900

    커뮤니티 글작성 레이아웃 작성

--- src/components/Post/WriteCommunityPost.jsx
+++ src/components/Post/WriteCommunityPost.jsx
@@ -8,17 +8,23 @@ const WriteCommunityPost = () => {
   return (
     <div className="wcp_div">
       {/* <ActionBtn aText="소중한 한마디를 전해주세요" /> */}
-      <div className="wcp_img_div">
-        <div className="wcp_img_add_btn">
-            <img src={CameraIcon} alt=""/>
-            <p>1/5</p>
+      <form action="" method="post">
+        <div className="wcp_img_div">
+            <div className="wcp_img_add_btn">
+                <img src={CameraIcon} alt=""/>
+                <p>1/5</p>
+            </div>
+            {/* <input type="image" src="https://via.placeholder.com/150" alt="" /> */}
         </div>
-        {/* <input type="image" src="https://via.placeholder.com/150" alt="" /> */}
-      </div>
-      <hr/>
-      <div className="wcp_wrtie_div">
-        <input type="text" placeholder="여기서부터 글을 작성해 주세요" name="" id="" />
-      </div>
+        <hr/>
+        <div className="wcp_write_div">
+            <textarea name="" id="" cols="30" rows="10" placeholder="여기서부터 글을 작성해 주세요"></textarea>
+        </div>
+        <hr/>
+        <div className="wcp_wrtie_btn">
+            <ActionBtn aText="게시글 등록하기"/>
+        </div>
+      </form>
     </div>
   );
 };
