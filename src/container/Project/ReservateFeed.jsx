605ffc8e802de5c94f7432efe6c8c26111e24a74
임동완 <limson_95@naver.com>
2020-02-26 13:32:11 +0900

    [ADD] bottomNav z-index [ISSUE] profile탭 아이템 없을때 오류

--- src/container/Project/ReservateFeed.jsx
+++ src/container/Project/ReservateFeed.jsx
@@ -4,9 +4,15 @@ import ProjectCardView from '@/components/ProjectCardView';
 const ReservateFeed = props => {
   return (
     <div>
-      {props.value.map(Data => {
-        return <ProjectCardView value={Data} type={'reservate'} />;
-      })}
+      {props.value.length === 0 ? (
+        <div className="not_show">
+          <p>표시할 피드가 없습니다.</p>
+        </div>
+      ) : (
+        props.value.map(Data => {
+          return <ProjectCardView value={Data} type={'reservate'} />;
+        })
+      )}
     </div>
   );
 };
