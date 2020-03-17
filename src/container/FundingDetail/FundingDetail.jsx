dc7533b82e356aa415533842c1369c20c6f3753a
임동완 <limson_95@naver.com>
2020-03-17 14:32:00 +0900

    [ADD] 통신 에러 핸들링

--- src/container/FundingDetail/FundingDetail.jsx
+++ src/container/FundingDetail/FundingDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import FundingDetailMain from '@/container/FundingDetail/FundingDetailMain';
 import FundingDetailStory from '@/container/FundingDetail/FundingDetailStory';
@@ -13,11 +13,14 @@ import BSContext from '@/context/bottomSheet';
 
 import FundingButton from '@/components/Funding/FundingButton';
 import Axios from 'axios';
+import { Redirect, useHistory } from 'react-router-dom';
 
 const FundingDetail = ({ match }) => {
+  const [data, setData] = useState('');
   const { action } = useContext(HABContext);
   const FDTab = useContext(FDTabContext);
   const BS = useContext(BSContext);
+  const history = useHistory();
 
   const url = 'http://localhost:7777/api/project/crud';
   // const url = 'http://3.135.237.171:7777/api/project/crud';
@@ -31,6 +34,8 @@ const FundingDetail = ({ match }) => {
   useEffect(() => {
     Axios.get(`${url}/${match.params.id}`).then(res => {
       console.log(res.data);
+      console.log(res.data.statusCode);
+      res.data.statusCode == 200 ? setData(res.data.ans) : history.push('/err');
     });
   }, []);
 
