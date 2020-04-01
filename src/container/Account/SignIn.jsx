9b6fab8073f55d28427cd4a7fa32e590da447cad
임동완 <limson_95@naver.com>
2020-04-01 15:28:29 +0900

    [DELETE] 주석

--- src/container/Account/SignIn.jsx
+++ src/container/Account/SignIn.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-// import axios from 'axios';
 import { useLocation, useHistory, Redirect } from 'react-router-dom';
 
 import resolveJWT from '@/lib/resolveJwt';
@@ -19,30 +18,7 @@ const SignIn = () => {
   const token = useQuery().get('token');
   const decodedToken = resolveJWT(token);
   const history = useHistory();
-  // const signInUri = 'http://localhost:7777/api/account/signIn';
-  const signInUri = 'http://3.135.237.171:7777/api/account/signIn';
   useEffect(() => {
-    // axios
-    //   .post(signInUri, {
-    //     id: decodedToken.id
-    //   })
-    //   .then(res => {
-    //     console.log(res.data.oid);
-    //     auth.action.setAuthToken(res.data.ans);
-    //     localStorage.setItem(
-    //       'token',
-    //       JSON.stringify({
-    //         token: res.data.ans
-    //       })
-    //     );
-    //     localStorage.setItem(
-    //       'oid',
-    //       JSON.stringify({
-    //         oid: res.data.oid
-    //       })
-    //     );
-    //     history.push(JSON.parse(localStorage.getItem('historyPath')).path);
-    //   });
     async function axios() {
       const ans = await signInAxios({ id: decodedToken.id });
       auth.action.setAuthToken(ans.data.ans);
